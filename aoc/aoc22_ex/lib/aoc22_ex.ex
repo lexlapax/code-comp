4fe290f577a593457fc286670226815c4035b4a1
Lex Lapax <lexlapax@gmail.com>
2022-12-16 10:36:04 -0800

    initial commit

---
+++ aoc/aoc22_ex/lib/aoc22_ex.ex
@@ -0,0 +1,18 @@
+defmodule Aoc22Ex do
+  @moduledoc """
+  Documentation for `Aoc22Ex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc22Ex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ aoc/aoc22_ex/test/aoc22_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc22ExTest do
+  use ExUnit.Case
+  doctest Aoc22Ex
+
+  test "greets the world" do
+    assert Aoc22Ex.hello() == :world
+  end
+end
